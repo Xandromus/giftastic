7d5c7752494dc814805bcf70d808ead052b0751a
Xandromus <xanderrapstine@gmail.com>
2018-03-04 17:24:49 -0600

    improved layout and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,20 @@
-var topics = ["nerds", "dorks", "dweebs"];
+var topics = ["michael bluth", "george bluth", "lucille bluth", "gob bluth", "lindsay fünke", "buster bluth", "george michael bluth", "maeby fünke", "tobias fünke", "oscar bluth"];
+
+var currIndex = topics.length,
+                    temporaryValue, randomIndex;
+
+                // While there remain elements to shuffle...
+                while (0 !== currIndex) {
+
+                    // Pick a remaining element...
+                    randomIndex = Math.floor(Math.random() * currIndex);
+                    currIndex -= 1;
+
+                    // And swap it with the current element.
+                    temporaryValue = topics[currIndex];
+                    topics[currIndex] = topics[randomIndex];
+                    topics[randomIndex] = temporaryValue;
+                }
 
 function renderButtons() {
 
@@ -24,12 +40,14 @@ $("#add-topic").on("click", function(event) {
     event.preventDefault();
 
     // This line will grab the text from the input box
-    var newTopic = $("#topic-input").val().trim();
+    var newTopic = $("#topic-input").val().trim().toLowerCase();
     // The movie from the textbox is then added to our array
-    topics.push(newTopic);
-
+    if (newTopic != "") {
+        topics.push(newTopic);
+    }
     // calling renderButtons which handles the processing of our movie array
     renderButtons();
+    $("#topic-input").val("");
 });
 
 $(document).on("click", "button", function() {
@@ -57,16 +75,16 @@ $(document).on("click", "button", function() {
             var rating = results[i].rating;
 
             // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + rating);
+            var p = $("<p>").text("Rating: " + rating.toUpperCase());
 
             // Creating an image tag
             var topicImage = $("<img class='gif'>");
 
             // Giving the image tag an src attribute of a proprty pulled off the
             // result item
-            topicImage.attr("src", results[i].images.fixed_width_still.url);
-            topicImage.attr("data-still", results[i].images.fixed_width_still.url);
-            topicImage.attr("data-animate", results[i].images.fixed_width.url);
+            topicImage.attr("src", results[i].images.original_still.url);
+            topicImage.attr("data-still", results[i].images.original_still.url);
+            topicImage.attr("data-animate", results[i].images.original.url);
             topicImage.attr("data-state", "still");
             topicImage.attr("alt", results[i].title);
 
@@ -82,19 +100,19 @@ $(document).on("click", "button", function() {
 });
 
 $(document).on("click", ".gif", function() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
-      } else {
+    } else {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
-      }
-    });
+    }
+});
 
 // Calling the renderButtons function at least once to display the initial list of movies
 renderButtons();
